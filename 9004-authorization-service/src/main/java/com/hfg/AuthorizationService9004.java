190bb56eb989ebe64f6629a70247efc5723f239c
hfg-yyds <1841814080@qq.com>
2022-04-05 21:39:20 +0800

    authorization服务

--- 9004-authorization-service/src/main/java/com/hfg/AuthorizationService9004.java
+++ 9004-authorization-service/src/main/java/com/hfg/AuthorizationService9004.java
@@ -3,6 +3,7 @@ package com.hfg;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @Author: Zero
@@ -11,6 +12,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 @Slf4j
+@EnableEurekaClient
 public class AuthorizationService9004 {
     public static void main(String[] args) {
         log.info("认证服务启动了,负责颁发token");
---
+++ 9004-authorization-service/src/main/java/com/hfg/config/OauthServerConfiger.java
@@ -0,0 +1,14 @@
+package com.hfg.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+
+/**
+ * @Author: Zero
+ * @Date: 2022/4/5 21:38
+ * @Description:
+ */
+@Configuration
+@EnableAuthorizationServer
+public class OauthServerConfiger {
+}
